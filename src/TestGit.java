9b907830de697f75b432e519830ed1cd3945a4fe
jinyan <1511583712@qq.com>
2019-04-21 19:02:50 +0800

    my first commit in eclipse

---
+++ src/TestGit.java
@@ -0,0 +1,11 @@
+import java.util.MissingFormatArgumentException;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("测试Git ing!");
+
+	}
+
+}
